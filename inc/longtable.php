c104d866635bd12abed9c1654658ff4d320102d5
komed3 <pk97@outlook.de>
2021-06-19 00:27:38 +0200

    new build engine for long table class

--- inc/longtable.php
+++ inc/longtable.php
@@ -2,8 +2,55 @@
     
     class Long_Table extends Table {
         
-        
+        public function build() {
+            
+            global $lng;
+            
+            $exGroups = [];
+            
+            $this->table .= '<table border="1" class="table ' . $this->property . '">';
+            
+            for( $p = 1; $p <= $this->maxP; $p++ ) {
+                
+                $this->table .= '<tr>';
+                
+                for( $g = 1; $g <= $this->maxG; $g++ ) {
+                    
+                    $field = $this->get_field( $p, $g );
+                    
+                    if( empty( $field ) ) {
+                        
+                        $this->table .= '<td class="empty">&nbsp;</td>';
+                        
+                    } else {
+                        
+                        foreach( $field as $e ) {
+                            
+                            $this->add_element( $p, $g, $e );
+                            
+                        }
+                        
+                        if( $g == 3 && count( $field ) < 2 ) {
+                            
+                            $this->table .= '<td class="empty" colspan="14">&nbsp;</td>';
+                            
+                        }
+                        
+                    }
+                                       
+                }
+                
+                $this->table .= '</tr>';
+                
+            }
+            
+        }
         
     }
+    
+    $lt = new Long_Table();
+    print $lt->output();
+    
+    exit;
     
 ?>
