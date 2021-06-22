c902f6f0ad447b01e8b171d6e30252d182a847e8
komed3 <pk97@outlook.de>
2021-06-22 13:42:09 +0200

    Update api.php

--- inc/api/api.php
+++ inc/api/api.php
@@ -2,9 +2,41 @@
     
     class API {
         
+        protected $CLI;
+        
+        protected $args;
+        
         function __construct() {
             
+            $this->CLI = ( php_sapi_name() === 'cli' );
             
+            $this->load_params();
+            
+        }
+        
+        protected function load_args() {
+            
+            if( $this->CLI ) {
+                
+                
+                
+            } else {
+                
+                foreach( [ 'action', 'elements', 'props' ] as $arg ) {
+                    
+                    $this->args[ $arg ] = !empty( $_REQUEST[ $arg ] )
+                        ? $_REQUEST[ $arg ]
+                        : null;
+                    
+                }
+                
+            }
+            
+        }
+        
+        public function is_CLI() {
+            
+            return $this->CLI;
             
         }
         
